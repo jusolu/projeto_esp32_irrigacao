dad1e9deff2ea75ce92a25c03eb55999c8ac82a2
jusolu <jusolu@gmail.com>
2026-07-21 22:43:48 -0400

    AddMinimalWiFiSketch

---
+++ esp32/esp32_wifi_minimo.ino
@@ -0,0 +1,29 @@
+#include <WiFi.h>
+
+const char* ssid     = "AP104-2.4G";
+const char* password = "papagaio";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado.");
+  Serial.println("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Nada no loop no exemplo mínimo
+}
